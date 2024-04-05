7546eb33afc1df284c43ac43e80b09ff4769e743
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2024-04-05 09:43:38 +0200

    El personaje principal rota desde un componente del juego

--- src/cpp/PlayerController.cpp
+++ src/cpp/PlayerController.cpp
@@ -1,4 +1,9 @@
 #include "PlayerController.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "LMVector.h"
+
+#include <iostream>
 
 using namespace JuegoDePistolas;
 using namespace LocoMotor;
@@ -18,6 +23,13 @@ void PlayerController::start()
 
 void PlayerController::update(float dT)
 {
+	std::cout << "PlayerControllerUpdate" << std::endl;
+
+	Transform* cubeTrnsf = _gameObject->getComponent<Transform>();
+
+	if (cubeTrnsf != nullptr)
+		cubeTrnsf->SetRotation(LMVector3(cubeTrnsf->GetRotation().GetX(),
+			cubeTrnsf->GetRotation().GetY() - 60 * dT / 1000, cubeTrnsf->GetRotation().GetZ()));
 }
 
 void JuegoDePistolas::PlayerController::setParameters(std::vector<std::pair<std::string, std::string>>& params)
