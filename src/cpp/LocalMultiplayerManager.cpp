eab068c273f671b683598bd4f8ed55dd6f104d34
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2024-04-07 13:10:13 +0200

    Estructura basica de LocalMultiplayerManager

--- src/cpp/LocalMultiplayerManager.cpp
+++ src/cpp/LocalMultiplayerManager.cpp
@@ -2,8 +2,11 @@
 #include "GameObject.h"
 #include "Transform.h"
 #include "LMVector.h"
+#include "MeshRenderer.h"
 
 #include <iostream>
+#include <SceneManager.h>
+#include <Scene.h>
 
 using namespace JuegoDePistolas;
 using namespace LocoMotor;
@@ -19,15 +22,68 @@ LocalMultiplayerManager::~LocalMultiplayerManager()
 
 void LocalMultiplayerManager::start()
 {
+	//for (int i = 0; i < allPlayers.size(); i++)
+	//	allPlayers[i] = nullptr;
 }
 
 void LocalMultiplayerManager::update(float dT)
 {
-	Transform* cubeTrnsf = _gameObject->getComponent<Transform>();
 
-	if (cubeTrnsf != nullptr)
-		cubeTrnsf->SetRotation(LMVector3(cubeTrnsf->GetRotation().GetX() + 60 * dT / 1000,
-			cubeTrnsf->GetRotation().GetY() + 60 * dT / 1000, cubeTrnsf->GetRotation().GetZ()));
+	// Check connections
+
+	std::list<Input::InputManager::ControllerId> controllersAdded = Input::InputManager::GetInstance()->getOnConnectControllers();
+	std::list<Input::InputManager::ControllerId> controllersRemoved = Input::InputManager::GetInstance()->getOnDisconnectControllers();
+
+	// Conexion de usuarios
+	for (const Input::InputManager::ControllerId& controllerId : controllersAdded) {
+
+		int playerIndex = -1;
+
+		// Calcular el index de jugador que tendra este mando
+		for (int i = 0; i < allPlayers.size(); i++)
+			if (allPlayers[0].playerController == nullptr) {
+				playerIndex = i;
+				break;
+			}
+
+
+		//// Crear el objeto jugador dependiendo del index del jugador
+		//GameObject* newPlayer = SceneManager::GetInstance()->getActiveScene()->addGameobject("Player_" + playerIndex);
+		//Transform* playerTransform = (Transform*)newPlayer->addComponent("Transform");
+
+		//MeshRenderer* playerMeshRenderer = (MeshRenderer*)newPlayer->addComponent("MeshRenderer");
+
+		//playerMeshRenderer->init("cubeMesh", "", false);
+		//playerMeshRenderer->setMesh("CubemanMesh.mesh");
+
+
+
+		//if (firstController == Input::InputManager::invalidControllerId())
+		//	firstController = controllerId;
+
+		//else if (secondController == Input::InputManager::invalidControllerId())
+		//	secondController = controllerId;
+
+		//else if (thirdController == Input::InputManager::invalidControllerId())
+		//	thirdController = controllerId;
+	}
+
+	// Desconexion de usuarios
+	for (const Input::InputManager::ControllerId& controllerId : controllersRemoved) {
+
+		//// Si se ha desconectado el primer usuario
+		//if (firstController == controllerId)
+		//	firstController = Input::InputManager::invalidControllerId();
+
+		//else if (secondController == controllerId)
+		//	secondController = Input::InputManager::invalidControllerId();
+
+		//else if (thirdController == controllerId)
+		//	thirdController = Input::InputManager::invalidControllerId();
+	}
+
+
+	std::cout << "LocalMultiplayerUpdate" << std::endl;
 }
 
 void LocalMultiplayerManager::setParameters(std::vector<std::pair<std::string, std::string>>& params)
--- src/include/LocalMultiplayerManager.h
+++ src/include/LocalMultiplayerManager.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Component.h"
+#include <unordered_map>
+#include "InputManager.h"
+#include "PlayerController.h"
+#include <utility>
+
+
+using namespace LocoMotor;
 
 namespace JuegoDePistolas {
+
 	class LocalMultiplayerManager : public LocoMotor::Component
 	{
 	public:
@@ -11,6 +19,16 @@ namespace JuegoDePistolas {
 		void start() override;
 		void update(float dT) override;
 		void setParameters(std::vector<std::pair<std::string, std::string>>& params) override;
+
+	private:
+
+		struct PlayerData {
+			Input::InputManager::ControllerId controllerId;
+			PlayerController* playerController;
+		};
+
+		// Stores all the player references by their controllerId
+		std::array<PlayerData, 4> allPlayers;
 	};
 }
 
