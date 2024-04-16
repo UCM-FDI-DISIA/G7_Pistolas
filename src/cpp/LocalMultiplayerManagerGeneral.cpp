6741b36a0b3010605b117234e7bb65ddcb19504c
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2024-04-16 21:32:17 +0200

    LocalMultiplayerManagerGeneral creado y añadido al juego

---
+++ src/cpp/LocalMultiplayerManagerGeneral.cpp
@@ -0,0 +1,46 @@
+#include "LocalMultiplayerManagerGeneral.h"
+#include "GameObject.h"
+#include "Transform.h"
+#include "LMVector.h"
+#include "MeshRenderer.h"
+
+#include <iostream>
+#include <SceneManager.h>
+#include <Scene.h>
+
+using namespace JuegoDePistolas;
+using namespace LocoMotor;
+
+LocalMultiplayerManagerGeneral* LocalMultiplayerManagerGeneral::_instance = nullptr;
+
+LocalMultiplayerManagerGeneral::LocalMultiplayerManagerGeneral()
+{ }
+
+LocalMultiplayerManagerGeneral::~LocalMultiplayerManagerGeneral()
+{ }
+
+LocalMultiplayerManagerGeneral* LocalMultiplayerManagerGeneral::GetInstance()
+{
+	return _instance;
+}
+
+std::array<Input::InputManager::ControllerId, 4> LocalMultiplayerManagerGeneral::getPlayers()
+{
+	if (this == nullptr)
+		return std::array<Input::InputManager::ControllerId, 4>();
+	else
+		return allPlayers;
+}
+
+void LocalMultiplayerManagerGeneral::init()
+{
+	_instance = this;
+
+	int currentPlayer = 0;
+	for (Input::InputManager::ControllerId& controllerId : allPlayers) {
+
+		// Marcar el id del controlador como invalido
+		controllerId = Input::InputManager::invalidControllerId();
+		currentPlayer++;
+	}
+}
---
+++ src/include/LocalMultiplayerManagerGeneral.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Component.h"
+#include <unordered_map>
+#include "InputManager.h"
+#include "LMVector.h"
+#include "PlayerController.h"
+#include <utility>
+
+
+using namespace LocoMotor;
+
+namespace JuegoDePistolas {
+
+	class LocalMultiplayerManagerGeneral
+	{
+	public:
+		LocalMultiplayerManagerGeneral();
+		~LocalMultiplayerManagerGeneral();
+
+		static LocalMultiplayerManagerGeneral* GetInstance();
+
+		void init();
+
+		std::array<Input::InputManager::ControllerId, 4> getPlayers();
+
+	private:
+
+		static LocalMultiplayerManagerGeneral* _instance;
+
+		// Stores all the player references by their controllerId
+		std::array<Input::InputManager::ControllerId, 4> allPlayers;
+	};
+}
+
+
+
