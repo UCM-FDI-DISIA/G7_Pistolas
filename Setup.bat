b2add0c7b878b22bfbb79d7f6e0fe49a3a0d210a
Javier V <javilleg@ucm.es>
2024-04-30 11:45:01 +0200

    set up arreglado

--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,18 @@
 set MotorBin=.\LocoMotor\bin
 set Bin=.\bin
 
-call .\LocoMotor\CompilarLocoMotor.bat
+cd LocoMotor
+
+call .\CompilarLocoMotor.bat
+
+cd ..
 
 msbuild G7_JuegoDePistolas.sln /p:configuration=Debug
 msbuild G7_JuegoDePistolas.sln /p:configuration=Release
 
 xcopy %MotorBin% %Bin% /h /i /c /k /e /r /y
-start %Bin%\LocoMotor_Main.exe
+
+cd bin
+
+start LocoMotor_Main.exe
+
