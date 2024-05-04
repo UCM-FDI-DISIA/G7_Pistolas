bf48cea7029f56ac12dae8199db3e2565de5e5d1
NachoDelCastillo <82290483+NachoDelCastillo@users.noreply.github.com>
2024-05-04 13:49:39 +0200

    Control de referencias nulas en la clase LocalMultiplayerManager

--- src/cpp/Bullet.cpp
+++ src/cpp/Bullet.cpp
@@ -21,7 +21,6 @@ using namespace LocoMotor;
 
 Bullet::Bullet()
 {
-
 }
 
 Bullet::~Bullet()
@@ -41,45 +40,29 @@ void JuegoDePistolas::Bullet::setBulletActive(bool _bulletActive)
 
 void Bullet::start()
 {
-	
 }
 
 void Bullet::update(float dT)
 {
 	// Si esta desactivado, no ejecutar el update
-	if (bulletActive == false)
+	if (!bulletActive)
 		return;
-	RigidBody* rb = _gameObject->getComponent<RigidBody>();
-	if (rb)rb->BeATrigger();
-	Transform* tr = _gameObject->getComponent<Transform>();
-	if (direction.magnitude() > .1f)
-	_gameObject->getComponent<RigidBody>()->SetLinearVelocity(direction * velocity * dT);
-	/*if (direction.magnitude() > .1f)
-		tr->setPosition(tr->getPosition() + direction * velocity * dT/1000);*/
 
-	//std::array<LocalMultiplayerManager::PlayerData, 4> allPlayers = LocalMultiplayerManager::GetInstance()->getPlayers();
+	if (_gameObject != nullptr) {
 
-	//allPlayers[0].gameObject;
+		RigidBody* rb = _gameObject->getComponent<RigidBody>();
+		if (rb != nullptr) {
+			rb->BeATrigger();
 
-	// Collision
-	//for (int i = 0; i < 4; ++i) {
+			if (direction.magnitude() > .1f)
+				rb->SetLinearVelocity(direction * velocity * dT);
+		}
 
-	//	// Primero comprobar si esta vivo antes de intentar matarlo otra vez
-	//	if (!GameplayManager::GetInstance()->isPlayerAlive(i))
-	//		continue;
-
-	//	// Si esta vivo, comprobar si esta lo suficientemente cerca de esta bala
-	//	GameObject* thisPlayerObj = allPlayers[i].gameObject;
-	//	float distance = LMVector3::distance(thisPlayerObj->getComponent<Transform>()->getPosition(), tr->getPosition());
-	//	if (LMVector3::distance(thisPlayerObj->getComponent<Transform>()->getPosition(), tr->getPosition()) < 3) {
-
-	//		thisPlayerObj->getComponent<PlayerController>()->bulletHit();
-	//	}
-	//}
-	timeAlive+=dT;
-	if (timeAlive>timeToLive) {
-		destroyBullet();
-		timeAlive = 0;
+		timeAlive += dT;
+		if (timeAlive > timeToLive) {
+			destroyBullet();
+			timeAlive = 0;
+		}
 	}
 }
 
@@ -89,26 +72,34 @@ void JuegoDePistolas::Bullet::setParameters(std::vector<std::pair<std::string, s
 
 void JuegoDePistolas::Bullet::awake()
 {
-	_gameObject->addComponent("RigidBody");
-	_gameObject->getComponent<RigidBody>()->SetMass(0.5);
-	_gameObject->getComponent<RigidBody>()->SetSize({ 1,0.2,1 });
+	if (_gameObject != nullptr) {
+		_gameObject->addComponent("RigidBody");
 
+		RigidBody* rb = _gameObject->getComponent<RigidBody>();
+		if (rb != nullptr) {
+			rb->SetMass(0.5);
+			rb->SetSize({ 1,0.2,1 });
+		}
+	}
 }
 
 
 void JuegoDePistolas::Bullet::destroyBullet() {
 	timeset = false;
-	LocoMotor::SceneManager::GetInstance()->getActiveScene()->removeGameobject(this->_gameObject->getName());
+
+	Scene* activeScene = LocoMotor::SceneManager::GetInstance()->getActiveScene();
+	if (activeScene != nullptr && _gameObject != nullptr)
+		activeScene->removeGameobject(this->_gameObject->getName());
 }
 
 void JuegoDePistolas::Bullet::OnCollisionEnter(GameObject* other)
 {
-	std::string otherName = other->getName();
-	std::cout << otherName << std::endl;
-	destroyBullet();
+	if (other != nullptr) {
+		std::string otherName = other->getName();
+		destroyBullet();
+	}
 }
 
 void JuegoDePistolas::Bullet::setVelocity(int vel) {
 	velocity = vel;
 }
-
--- src/cpp/LocalMultiplayerManager.cpp
+++ src/cpp/LocalMultiplayerManager.cpp
@@ -17,7 +17,6 @@ LocalMultiplayerManager* LocalMultiplayerManager::_instance = nullptr;
 
 LocalMultiplayerManager::LocalMultiplayerManager()
 {
-
 }
 
 LocalMultiplayerManager::~LocalMultiplayerManager()
@@ -40,6 +39,7 @@ std::array<LocalMultiplayerManager::PlayerData, 4> LocalMultiplayerManager::getP
 int JuegoDePistolas::LocalMultiplayerManager::getNumPlayersConnected()
 {
 	int numPlayers = Input::InputManager::GetInstance()->getCurrentlyConnectedControllers().size();
+
 	if (numPlayers > 4)
 		numPlayers = 4;
 	return numPlayers;
@@ -49,12 +49,19 @@ void JuegoDePistolas::LocalMultiplayerManager::awake()
 {
 	_instance = this;
 
+	Scene* activeScene = SceneManager::GetInstance()->getActiveScene();
+
+	if (activeScene == nullptr)
+		return;
+
 	// Asignar referencias de jugadores
 
-	allPlayers[0].gameObject = SceneManager::GetInstance()->getActiveScene()->getObjectByName("cube");
-	allPlayers[1].gameObject = SceneManager::GetInstance()->getActiveScene()->getObjectByName("Player_2");
-	allPlayers[2].gameObject = SceneManager::GetInstance()->getActiveScene()->getObjectByName("Player_3");
-	allPlayers[3].gameObject = SceneManager::GetInstance()->getActiveScene()->getObjectByName("Player_4");
+	if (allPlayers.size() > 3) {
+		allPlayers[0].gameObject = activeScene->getObjectByName("cube");
+		allPlayers[1].gameObject = activeScene->getObjectByName("Player_2");
+		allPlayers[2].gameObject = activeScene->getObjectByName("Player_3");
+		allPlayers[3].gameObject = activeScene->getObjectByName("Player_4");
+	}
 
 	int currentPlayer = 0;
 	for (PlayerData& playerData : allPlayers) {
@@ -63,57 +70,78 @@ void JuegoDePistolas::LocalMultiplayerManager::awake()
 		playerData.controllerId = Input::InputManager::invalidControllerId();
 
 		// Asignar referenciar de la clase PlayerController
-		playerData.playerController = playerData.gameObject->getComponent<PlayerController>();
-		playerData.playerController->setPlayerId(currentPlayer);
+		if (playerData.gameObject != nullptr) {
 
-		// Desactivar el objeto
-		playerData.playerController->setPlayerActive(false);
-		//playerData.gameObject->setActive(false);
-		//playerData.gameObject->getComponent<MeshRenderer>()->setEnabled(false);
-		//playerData.gameObject->getComponent<ParticleSystem>()->setEnabled(false);
+			playerData.playerController = playerData.gameObject->getComponent<PlayerController>();
+
+			if (playerData.playerController != nullptr) {
+				playerData.playerController->setPlayerId(currentPlayer);
+
+				// Desactivar el objeto
+				playerData.playerController->setPlayerActive(false);
+			}
+		}
 
 		currentPlayer++;
 	}
 
-	// Temporal, arreglar lo de que el update siempre se llame despues de cualquier start
-	Scene* scene = SceneManager::GetInstance()->getActiveScene();
-	spawnPoints = {
-		scene->getObjectByName("CharacterSpawnpoint_1")->getComponent<Transform>()->getPosition(),
-		scene->getObjectByName("CharacterSpawnpoint_2")->getComponent<Transform>()->getPosition(),
-		scene->getObjectByName("CharacterSpawnpoint_3")->getComponent<Transform>()->getPosition(),
-		scene->getObjectByName("CharacterSpawnpoint_4")->getComponent<Transform>()->getPosition(),
-	};
+
+	GameObject* CharacterSpawnpoint_1 = activeScene->getObjectByName("CharacterSpawnpoint_1");
+	GameObject* CharacterSpawnpoint_2 = activeScene->getObjectByName("CharacterSpawnpoint_2");
+	GameObject* CharacterSpawnpoint_3 = activeScene->getObjectByName("CharacterSpawnpoint_3");
+	GameObject* CharacterSpawnpoint_4 = activeScene->getObjectByName("CharacterSpawnpoint_4");
+
+	if (CharacterSpawnpoint_1 != nullptr && CharacterSpawnpoint_2 != nullptr
+		&& CharacterSpawnpoint_3 != nullptr && CharacterSpawnpoint_4 != nullptr) {
+
+		Transform* CharacterSpawnpoint_1_transform = CharacterSpawnpoint_1->getComponent<Transform>();
+		Transform* CharacterSpawnpoint_2_transform = CharacterSpawnpoint_2->getComponent<Transform>();
+		Transform* CharacterSpawnpoint_3_transform = CharacterSpawnpoint_3->getComponent<Transform>();
+		Transform* CharacterSpawnpoint_4_transform = CharacterSpawnpoint_4->getComponent<Transform>();
+
+		if (CharacterSpawnpoint_1_transform != nullptr && CharacterSpawnpoint_2_transform != nullptr
+			&& CharacterSpawnpoint_3_transform != nullptr && CharacterSpawnpoint_4_transform != nullptr) {
+
+			spawnPoints = {
+				CharacterSpawnpoint_1_transform->getPosition(),
+				CharacterSpawnpoint_2_transform->getPosition(),
+				CharacterSpawnpoint_3_transform->getPosition(),
+				CharacterSpawnpoint_4_transform->getPosition(),
+			};
+		}
+	}
 
 	//  Mostrar los players de los mandos ya conectados al entrar a la escena
 	std::list<LocoMotor::Input::InputManager::ControllerId>controllers = Input::InputManager::GetInstance()->getCurrentlyConnectedControllers();
 	int it = 0;
 	for (auto contrID : controllers) {
+
 		// No se permitiran mas de 4 mandos conectados al mismo tiempo
-		if (it > 4)
+		if (it > 4 || allPlayers.size() <= it)
 			break;
+
 		allPlayers[it].controllerId = contrID;
 
-		// Activar el objeto jugador dependiendo del index del jugador
-		allPlayers[it].playerController->setPlayerActive(true);
-		//allPlayers[it].gameObject->setActive(true);
-		//if (allPlayers[it].gameObject->getComponent<MeshRenderer>() != nullptr)
-		//	allPlayers[it].gameObject->getComponent<MeshRenderer>()->setEnabled(true);
+		if (allPlayers[it].playerController != nullptr) {
+			// Activar el objeto jugador dependiendo del index del jugador
+			allPlayers[it].playerController->setPlayerActive(true);
+			// Asignar a la clase PlayerController, el controllerId necesario para vincularlo con el mando
+			allPlayers[it].playerController->setControllerId(contrID);
+		}
 
-		//if (allPlayers[it].gameObject->getComponent<ParticleSystem>() != nullptr)
-		//	allPlayers[it].gameObject->getComponent<ParticleSystem>()->setEnabled(true);
+		if (allPlayers[it].gameObject != nullptr && allPlayers[it].gameObject->getComponent<Transform>() != nullptr) {
 
-		if (allPlayers[it].gameObject->getComponent<Transform>() != nullptr)
-			allPlayers[it].gameObject->getComponent<Transform>()->setPosition(spawnPoints[it]);
+			if (spawnPoints.size() > it)
+				allPlayers[it].gameObject->getComponent<Transform>()->setPosition(spawnPoints[it]);
+		}
 
-		// Asignar a la clase PlayerController, el controllerId necesario para vincularlo con el mando
-		allPlayers[it].playerController->setControllerId(contrID);
+
 		it++;
 	}
 }
 
 void LocalMultiplayerManager::start()
 {
-	
 }
 
 void LocalMultiplayerManager::update(float dT)
@@ -152,18 +180,29 @@ void LocalMultiplayerManager::update(float dT)
 		if (playerIndex < 0 || playerIndex > 4)
 			break;
 
+		if (allPlayers.size() <= playerIndex)
+			break;
+
 		// Asignar nuevo controllerId
 		allPlayers[playerIndex].controllerId = controllerId;
 
 		// Activar el objeto jugador dependiendo del index del jugador
-		allPlayers[playerIndex].gameObject->setActive(true);
-		allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>()->setEnabled(true);
-		allPlayers[playerIndex].gameObject->getComponent<ParticleSystem>()->setEnabled(true);
+		if (allPlayers[playerIndex].gameObject != nullptr) {
+			allPlayers[playerIndex].gameObject->setActive(true);
 
-		allPlayers[playerIndex].gameObject->getComponent<Transform>()->setPosition(spawnPoints[playerIndex]);
+			if (allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>() != nullptr)
+				allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>()->setEnabled(true);
+
+			if (allPlayers[playerIndex].gameObject->getComponent<ParticleSystem>() != nullptr)
+				allPlayers[playerIndex].gameObject->getComponent<ParticleSystem>()->setEnabled(true);
+
+			if (allPlayers[playerIndex].gameObject->getComponent<Transform>() != nullptr && spawnPoints.size() > playerIndex)
+				allPlayers[playerIndex].gameObject->getComponent<Transform>()->setPosition(spawnPoints[playerIndex]);
+		}
 
 		// Asignar a la clase PlayerController, el controllerId necesario para vincularlo con el mando
-		allPlayers[playerIndex].playerController->setControllerId(controllerId);
+		if (allPlayers[playerIndex].playerController != nullptr)
+			allPlayers[playerIndex].playerController->setControllerId(controllerId);
 	}
 
 	// Desconexion de usuarios
@@ -184,15 +223,23 @@ void LocalMultiplayerManager::update(float dT)
 		if (playerIndex < 0 || playerIndex > 4)
 			break;
 
+		if (allPlayers.size() <= playerIndex)
+			break;
+
 		// Invalidar el controllerId
 		allPlayers[playerIndex].controllerId = Input::InputManager::invalidControllerId();
 
 		// Desactivar el objeto jugador dependiendo del index del jugador
-		allPlayers[playerIndex].gameObject->setActive(false);
-		allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>()->setEnabled(false);
+		if (allPlayers[playerIndex].gameObject != nullptr) {
+			allPlayers[playerIndex].gameObject->setActive(false);
+
+			if (allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>() != nullptr)
+				allPlayers[playerIndex].gameObject->getComponent<MeshRenderer>()->setEnabled(false);
+		}
 
 		// Borrar la referencia a la clase PlayerController
-		allPlayers[playerIndex].playerController->setControllerId(Input::InputManager::invalidControllerId());
+		if (allPlayers[playerIndex].playerController != nullptr)
+			allPlayers[playerIndex].playerController->setControllerId(Input::InputManager::invalidControllerId());
 	}
 }
 
